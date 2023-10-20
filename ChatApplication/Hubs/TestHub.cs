9f3df8f6764c1cd286f883bc9f755ac7a7b16940
Shuddhatm Choudhary <shuchoudhary.microsoft.com>
2023-10-20 14:55:39 +0530

    Add project files.

---
+++ ChatApplication/Hubs/TestHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApplication.Hubs
+{
+    public class TestHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine($"Connected to client - {Context.ConnectionId}");
+            return base.OnConnectedAsync();
+        }
+    }
+}
---
+++ ChatApplication/wwwroot/js/testhub.js
@@ -0,0 +1,13 @@
+﻿// create connection
+var connection = new signalR.HubConnectionBuilder().withUrl("/hubs/testhub").build();
+
+
+function fufilled() {
+    console.log("connection to test hu established");
+}
+
+function rejected() {
+
+}
+
+connection.start().then(fufilled, rejected);
