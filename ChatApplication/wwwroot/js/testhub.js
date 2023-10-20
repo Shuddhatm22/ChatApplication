11d6c446cecdd18fb8b0ebacda7551ebe59b9e44
Shuddhatm Choudhary <shuchoudhary.microsoft.com>
2023-10-20 15:41:54 +0530

    test hub connection

--- ChatApplication/wwwroot/js/testhub.js
+++ ChatApplication/wwwroot/js/testhub.js
@@ -1,13 +1,25 @@
-﻿// create connection
-var connection = new signalR.HubConnectionBuilder().withUrl("/hubs/testhub").build();
+﻿var usernameForm = document.getElementById("usernameForm");
+usernameForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    var usernameInput = document.getElementById("username");
+    var userameValue = usernameInput.value;
+    console.log("hello " + userameValue);
+    testConnection();
+})
+
+function testConnection() {
+    // create connection
+    var connection = new signalR.HubConnectionBuilder().withUrl("/hubs/testhub").build();
+    connection.start().then(fufilled, rejected);
+}
 
 
 function fufilled() {
-    console.log("connection to test hu established");
+    console.log("connection to test hub established");
 }
 
 function rejected() {
-
+    console.log("connection failed");
 }
 
-connection.start().then(fufilled, rejected);
